00c8044c8c8dee4e0d63c7efb3816e02b674a5af
mmvschaik <mmvschaik@gmail.com>
2021-07-13 09:42:41 +0200

    Add conditional styling

--- src/components/create-area.jsx
+++ src/components/create-area.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom';
 
 function CreateArea(props) {
     const [note, updateNote] = useState({title: "", content: ""});
+    const [isExpanded, setExpanded] = useState(false);
 
     function noteChange(e) {
         const {name, value} = e.target;
@@ -14,12 +18,34 @@ function CreateArea(props) {
         e.preventDefault();
     }
 
+    function expand() {
+        setExpanded(true);
+    }
+
     return (
         <div>
-            <form>
-                <input onChange={noteChange} type="text" name="title" placeholder="Title" value={note.title} />
-                <textarea onChange={noteChange} name="content" rows="3" placeholder="Take a note..." value={note.content} />
-                <button onClick={submit}>Add</button>
+            <form className="create-note">
+                {isExpanded &&
+                <input
+                    onChange={noteChange}
+                    type="text"
+                    name="title"
+                    placeholder="Title"
+                    value={note.title}
+                />}
+                <textarea
+                    onClick={expand}
+                    onChange={noteChange}
+                    name="content"
+                    placeholder="Take a note..."
+                    value={note.content}
+                    rows={isExpanded ? "3" : "1"}
+                />
+                <Zoom in={isExpanded}>
+                    <Fab onClick={submit}>
+                        <AddIcon />
+                    </Fab>
+                </Zoom>
             </form>
         </div>
     );
--- src/components/note.jsx
+++ src/components/note.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 function Note(props) {
     return (
         <div className="note">
             <h2>{props.title}</h2>
             <p>{props.content}</p>
-            <button onClick={() => props.deleteNote(props.id)}>DELETE</button>
+            <button onClick={() => props.deleteNote(props.id)}><DeleteIcon /></button>
         </div>
     )
 }
